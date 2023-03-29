c8fff9564ffd0f18f7f50556e17f04b678738b15
saibanu katlakunta <123714004+saibanu2023@users.noreply.github.com>
2023-03-29 20:05:12 +0530

    Day03
    Drag and Drop

---
+++ src/day03/Program01.java
@@ -0,0 +1,27 @@
+package day03;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Program01 {
+
+	public static void main(String[] args) { 
+		WebDriver driver;
+		driver = new EdgeDriver ();
+		driver.get("https://demo.guru99.com/test/drag_drop.html");
+		
+		
+		WebElement src = driver.findElement(By.xpath("//li[@id='credit2']"));
+				
+		WebElement target = driver.findElement(By.xpath("//ol[@id='bank']"));
+		
+		Actions action= new Actions(driver);
+		action.dragAndDrop(src, target).build().perform();
+		
+
+	}
+
+}
